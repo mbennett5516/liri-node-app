8a0ba96e0cff4b8891eb7807e8dbbb2360be9c2e
mbennett <mbennett5516@yahoo.com>
2018-10-15 18:36:58 -0500

    fixed dependency order

--- liri.js
+++ liri.js
@@ -5,6 +5,8 @@ const Spotify = require('node-spotify-api');
 
 const env = require('dotenv').config();
 
+const keys = require('./keys.js');
+
 const spotify = new Spotify(keys.spotify);
 
 const action = process.argv[2];
